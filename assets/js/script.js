52d4ca5fafd31bc6894c251f26620f169db11070
Casey Dwyer <caseyjdwyer@gmail.com>
2022-12-06 17:20:10 -0800

    Add current date into the page header

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,10 @@
+// Global variables
+
+// Get current time
+var currentDay = dayjs().format("MMMM DD, YYYY");
+
+console.log(currentDay);
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
@@ -18,6 +25,7 @@ $(function () {
   // 4. TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
-  // 1. TODO: Add code to display the current date in the header of the page.
+  
+  // Code to display the current date in the header of the page
+  $("#currentDay").text(currentDay);
 });
