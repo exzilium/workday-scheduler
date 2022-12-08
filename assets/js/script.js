c3148856fb028443cc3925cf4e4d28eb8e920361
Casey Dwyer <caseyjdwyer@gmail.com>
2022-12-07 21:34:58 -0800

    Add logic framework for checking current time vs scheduler time and performing an action based on past, present, future state

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,18 @@
 
 // Get current time
 var currentDay = dayjs().format("MMMM DD, YYYY");
+var currentHour = dayjs().format("H");
 
-console.log(currentDay);
+currentHour = 14;
+// Number of timeBlocks (it is a static 9, but setting variable for future scalability)
+// var timeBlockCount = $(".time-block").length;
+// var timeBlockArray = [$(".time-block")];
 
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+  console.log("ready!");
   // 2. TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -21,11 +26,23 @@ $(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
+  $(".time-block").each(function () {
+    // Time block hour > real current hour?
+    if ($(this).data("hour") > currentHour) {
+      console.log($(this).data("hour") + " > " + currentHour);
+
+      // ...less than real current hour
+    } else if ($(this).data("hour") < currentHour) {
+      console.log($(this).data("hour") + " < " + currentHour);
+
+      // ...equal to real current hour
+    } else console.log($(this).data("hour") + " = " + currentHour);
+  });
+
   // 4. TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  
+
   // Code to display the current date in the header of the page
   $("#currentDay").text(currentDay);
 });
